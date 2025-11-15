c7d825c81de869766650634d0fe85028346d60ee
Saitama26 <gamerrayx851@gmail.com>
2025-11-15 16:00:07 +0300

    adding day5 task solving

---
+++ src/Day5/Task1/Algorithm.cs
@@ -0,0 +1,70 @@
+﻿namespace Day5.Task1;
+
+public static partial class Algorithm
+{
+    public static IEnumerable<int> ParseString(IEnumerable<string> inputs, int numberDegree)
+    {
+        if (inputs == null)
+        {
+            throw new ArgumentNullException($"{nameof(inputs)} can't be null");
+        } 
+        if (numberDegree < 2 || numberDegree > 16)
+        {
+            throw new ArgumentOutOfRangeException($"{nameof(numberDegree)} is out of range");
+        }
+
+        var results = new List<int>();
+
+        foreach (var input in inputs)
+        {
+            if(string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentException($"{nameof(input)} can't be empty");
+            }
+
+            var str = input.Trim();
+            var negative = str.StartsWith("-");
+            
+            if (negative)
+            {
+                str = str.Substring(1);
+            }
+
+            var value = 0;
+
+            for (var index = 0; index < str.Length; index++) 
+            {
+                var digit = CharToDigit(str[index]);
+                if(digit > numberDegree)
+                {
+                    throw new ArgumentException($"{nameof(numberDegree)}  is unacceptable for the numeral system. Number degree is {numberDegree}");
+                }
+
+                checked
+                {
+                    value += digit * (int)Math.Pow(numberDegree, str.Length - 1 - index);
+                }
+            }
+            if (negative) 
+            { 
+                value = -value; 
+            }
+
+            results.Add(value);
+        }
+
+        return results;
+    }
+
+    private static int CharToDigit(char c)
+    {
+        if (c >= '0' && c <= '9')
+            return c - '0';
+        if (c >= 'A' && c <= 'F')
+            return 10 + (c - 'A');
+        if (c >= 'a' && c <= 'f')
+            return 10 + (c - 'a');
+
+        throw new ArgumentException($"Недопустимый символ '{c}' в числе.");
+    }
+}
---
+++ src/Day5/Task2/Queue.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+namespace Day5.Task2;
+
+public class Queuee<T> : IEnumerable<T>
+{
+    private readonly List<T> _queue;
+    public int Count { get => _queue.Count; }
+
+    public Queuee()
+    {
+        _queue = new List<T>();
+    }
+
+    public T Dequeue()
+    {
+        if (_queue.Count == 0)
+        {
+            throw new InvalidOperationException($"Queue is empty");
+        }
+
+        var elem = _queue[0];
+        _queue.RemoveAt(0);
+        return elem;
+    }
+
+    public T Peek()
+    {
+        if (_queue.Count == 0)
+        {
+            throw new InvalidOperationException("Queue is empty");
+        }
+
+        return _queue[0];
+    }
+
+    public void Enqueue(T item) => _queue.Add(item);
+
+    public bool Contains(T item) => _queue.Contains(item);
+
+    public void Clear() => _queue.Clear();
+        
+    public IEnumerator<T> GetEnumerator() => _queue.GetEnumerator();
+    
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
