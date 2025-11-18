374e78127c87eec347a52d4316a060fe8d423331
Saitama26 <gamerrayx851@gmail.com>
2025-11-18 18:29:51 +0300

    adding fixtures, mock and changing tests

---
+++ tests/Day4.Tests/Task1/Fixtures/TimeMeasurerFixture.cs
@@ -0,0 +1,15 @@
+﻿using Day4.Task1.Implementation;
+
+namespace Day4.Tests.Task1.Fixtures;
+
+public class TimeMeasurerFixture
+{
+    public ConsoleTimeMeasurer consoleMeasurer { get; }
+    public FileLoggingTimeMeasurer fileMeasurer { get; }
+
+    public TimeMeasurerFixture()
+    {
+        consoleMeasurer = new ConsoleTimeMeasurer();
+        fileMeasurer = new FileLoggingTimeMeasurer("D:\\log.txt");
+    }
+}
---
+++ tests/Day4.Tests/Task1/TimedGcdAlgorithmTests.cs
@@ -0,0 +1,71 @@
+﻿using Day4.Task1.Implementation;
+using Day4.Tests.Task1.Fixtures;
+
+namespace Day4.Tests.Task1;
+
+public class TimedGcdAlgorithmTests : IClassFixture<GcdFixture>, IClassFixture<TimeMeasurerFixture>
+{
+    private readonly GcdFixture _gcdFixture;
+    private readonly TimeMeasurerFixture _timeFixture;
+
+    public TimedGcdAlgorithmTests(GcdFixture gcdFixture, TimeMeasurerFixture timeMeasurerFixture)
+    {
+        _gcdFixture = gcdFixture;
+        _timeFixture = timeMeasurerFixture;
+    }
+
+    [Fact]
+    public void Constructor_NullAlgorithm_ThrowsArgumentNullException_WhenAlgorithmIsNull()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new TimedGcdAlgorithm(null, _timeFixture.consoleMeasurer));
+    }
+
+    [Fact]
+    public void Constructor_NullTimeMeasurer_ThrowsArgumentNullException_WhenMeasurerIsNull()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new TimedGcdAlgorithm(_gcdFixture.Euclidean, null));
+    }
+
+    [Fact]
+    public void Calculate_UsesEuclideanAlgorithm_ReturnsCorrectResult()
+    {
+        // Arrange
+        var timedAlgorithm = new TimedGcdAlgorithm(_gcdFixture.Euclidean, _timeFixture.consoleMeasurer);
+
+        // Act
+        var result = timedAlgorithm.Calculate(252, 105);
+
+        // Assert
+        Assert.Equal(21, result);
+    }
+
+    [Fact]
+    public void Calculate_UsesBinaryAlgorithm_ReturnsCorrectResult()
+    {
+        // Arrange
+        var timedAlgorithm = new TimedGcdAlgorithm(_gcdFixture.Binary, _timeFixture.consoleMeasurer);
+
+        // Act
+        var result = timedAlgorithm.Calculate(252, 105);
+
+        // Assert
+        Assert.Equal(21, result);
+    }
+
+    [Fact]
+    public void Calculate_LogsExecutionTimeToFile()
+    {
+        // Arrange
+        var timedAlgorithm = new TimedGcdAlgorithm(_gcdFixture.Euclidean, _timeFixture.fileMeasurer);
+
+        // Act
+        var result = timedAlgorithm.Calculate(252, 105);
+
+        // Assert
+        Assert.Equal(21, result);
+
+        Assert.True(File.Exists("D:\\log.txt"));
+    }
+}
