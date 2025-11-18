6d21c207b15f70cf4f56fea08d86a7c87d64c896
Saitama26 <gamerrayx851@gmail.com>
2025-11-18 18:24:44 +0300

    refactoring

--- src/Day4/Task2/Interfaces/IBookService.cs
+++ src/Day4/Task2/Interfaces/IBookService.cs
@@ -5,8 +5,8 @@ namespace Day4.Task2.Interfaces;
 
 public interface IBookService
 {
-    public void Add(Book book);
-    public void Remove(Book book);
-    public IEnumerable<Book> FindByTag(BookTag tag, object value);
-    public IEnumerable<Book> SortBy(BookTag tag);
+    void Add(Book book);
+    void Remove(Book book);
+    IEnumerable<Book> FindByTag(BookTag tag, object value);
+    IEnumerable<Book> SortBy(BookTag tag);
 }
