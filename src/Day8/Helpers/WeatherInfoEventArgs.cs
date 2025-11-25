184ba084adfd5f05baff60fab559a105d008305f
Saitama26 <gamerrayx851@gmail.com>
2025-11-25 17:47:55 +0300

    adding helpers

---
+++ src/Day8/Helpers/WeatherInfoEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace Day8.Helpers;
+
+public class WeatherInfoEventArgs
+{
+    public float Temperature { get; }
+    public float Humidity { get; }
+    public float Pressure { get; }
+
+    public WeatherInfoEventArgs(float temperature, float humidity, float pressure)
+    {
+        Temperature = temperature;
+        Humidity = humidity;
+        Pressure = pressure;
+    }
+}
